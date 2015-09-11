71a3791e887d26814012be822c71155b4d409b52
Lê Trọng Lợi <trongloikt192@gmail.com>
2015-09-11 15:21:40 +0000

    add Contact model

---
+++ app/models/Contact.php
@@ -0,0 +1,14 @@
+<?php
+
+class Customer extends \Eloquent {
+	protected $table = 'contacts';
+
+	protected $fillable = ['status'];
+
+	public static $rules = [
+		'name' => 'required|min:3'
+		, 'address' => 'required|min:10'
+		, 'email' => 'required|min:3'
+		, 'phone_number' => 'required|min:3'
+	];
+}
